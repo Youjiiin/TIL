906998add5f0d704957f5a71b0ee0865f0107455
Youjiiin <yujinjang24@gmail.com>
2024-06-18 17:20:07 +0900

    React with TS: 컴포넌트 타입 지정하기

---
+++ react_with_ts/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+import styles from './Button.module.css';
+
+interface Props {
+    className?: string;
+    id?: string;
+    children?: ReactNode;
+    onClick: any;
+}
+
+export default function Button({ className = '', id, children, onClick }: Props) {
+  const classNames = `${styles.button} ${className}`;
+  return <button className={classNames}>
+        {children}
+    </button>;
+}
