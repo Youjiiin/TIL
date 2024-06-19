9e9bd92bbd25fde8e4b9eb4cc7b27b2ac2dcfecf
Youjiiin <yujinjang24@gmail.com>
2024-06-19 14:21:55 +0900

    React with TS: 이벤트 핸들러

--- react_with_ts/src/App.tsx
+++ react_with_ts/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { ChangeEvent, MouseEvent, useEffect, useRef, useState } from 'react';
 import './App.css';
 import Button from './components/Button';
 import Input from './components/Input';
@@ -24,7 +24,7 @@ function App() {
     if (form) form['username'].focus();
   }, []);
 
-  function handleChange(e: any) {
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
     const { name, value } = e.target;
     const nextValues = {
       ...values,
@@ -33,7 +33,8 @@ function App() {
     setValues(nextValues);
   }
 
-  function handleClick(e: any) {
+  //e: SyntheticEvent도 사용가능 : 리액트 이벤트 객체의 조상 격의 타입
+  function handleClick(e: MouseEvent) {
     e.preventDefault();
 
     const message = `${values.username}님 환영합니다`;
--- react_with_ts/src/components/Button.tsx
+++ react_with_ts/src/components/Button.tsx
@@ -1,16 +1,17 @@
-import { ReactNode } from 'react';
+import { MouseEvent, MouseEventHandler, ReactNode } from 'react';
 import styles from './Button.module.css';
 
 interface Props {
     className?: string;
     id?: string;
     children?: ReactNode;
-    onClick: any;
+    //onClick: MouseEventHandler<HTMLButtonElement>;
+    onClick: (e: MouseEvent) => void;
 }
 
 export default function Button({ className = '', id, children, onClick }: Props) {
   const classNames = `${styles.button} ${className}`;
-  return <button className={classNames}>
+  return <button className={classNames} id={id} onClick={onClick}>
         {children}
     </button>;
 }
