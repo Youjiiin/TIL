0ea3f68e39126493ec6fc219e01af96a4ae30d27
Youjiiin <yujinjang24@gmail.com>
2024-08-27 14:06:20 +0900

    tailwindcss: 기본 환경설정

---
+++ TailwindCSS/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.html"],
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
